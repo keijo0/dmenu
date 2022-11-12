9eb0a97a83c0595959ad62bc8b543c3eae56bb42
keijo0 <95128521+keijo0@users.noreply.github.com>
2022-11-12 02:18:18 +0200

    Add files via upload

--- config.h
+++ config.h
@@ -1,10 +1,8 @@
-/* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
-
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 700;                    /* minimum width when centered */
 static const char *fonts[] = {
-	"Cousine:size=8"
+	"Cousine:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -13,11 +11,6 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#beaa9b", "#161616" },
 	[SchemeOut] = { "#beaa9b", "#161616" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
 static const char worddelimiters[] = " ";
