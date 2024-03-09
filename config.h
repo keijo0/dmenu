f5fa144585248a00bfbb1514d90d22f9f0f190aa
0 <0@01x0.com>
2024-03-09 20:59:15 +0000

    MENU FOR D

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
-static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 800;                    /* minimum width when centered */
 static const char *fonts[] = {
-	"Cousine:size=8"
+	"Azeret Mono:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#beaa9b", "#161616" },
-	[SchemeSel] = { "#beaa9b", "#161616" },
-	[SchemeOut] = { "#beaa9b", "#161616" },
+	[SchemeNorm] = { "#beaa9b", "#101010" },
+	[SchemeSel] = { "#beaa9b", "#101010" },
+	[SchemeOut] = { "#beaa9b", "#101010" },
 };
-static unsigned int lines      = 10;
+static unsigned int lines      = 0;
 
-static const char worddelimiters[] = " ";
+static const char worddelimiters[] = "";
