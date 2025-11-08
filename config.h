58bf1279c710099b4b03e9a06bb3b7fab6d47ac0
keijo0 <0@01x0.com>
2025-11-08 14:46:33 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -2,15 +2,18 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 800;                    /* minimum width when centered */
 static const char *fonts[] = {
-	"Azeret Mono:size=7"
+	"Fixedsys:size=9"
 };
-static const char *prompt      = "" ;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = ">" ;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
+//	[SchemeNorm] = { "#000000", "#beaa9b" },
+//	[SchemeSel] = { "#000000", "#beaa9b" },
+//	[SchemeOut] = { "#000000", "#beaa9b" },
 	[SchemeNorm] = { "#beaa9b", "#101010" },
 	[SchemeSel] = { "#beaa9b", "#101010" },
 	[SchemeOut] = { "#beaa9b", "#101010" },
 };
-static unsigned int lines      = 20;
+static unsigned int lines      = 30;
 
 static const char worddelimiters[] = "";
